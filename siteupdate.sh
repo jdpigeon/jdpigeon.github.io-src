94e81680834d6517d8de08686cd234eb1fa94285
jdpigeon <morrisondano@gmail.com>
2016-03-23 14:30:21 -0400

    added No Choice and Industriousness'

---
+++ siteupdate.sh
@@ -0,0 +1,9 @@
+cd output
+git add .
+git commit -m "First post."
+git push -u origin master
+cd ..
+echo '*.pyc' >> .gitignore #don't need pyc files
+git add .
+git commit -m "First commit."
+git push -u origin master
